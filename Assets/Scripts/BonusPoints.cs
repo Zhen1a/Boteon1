482784d9cce0832d859e8e1187ddd017c7297a12
unknown <zhenia200513@gmail.com>
2019-11-16 11:20:20 +0300

    First commit

---
+++ Assets/Scripts/BonusPoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusPoints : MonoBehaviour {
+	//make a container for the heads up display
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+
+		}
+
+	}
+}
